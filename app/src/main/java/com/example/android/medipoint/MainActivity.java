792f69fc5a526efe8c0a2d1d9ef8f8be17f8084e
Ayushmaan sharma <55322319+ayushman999@users.noreply.github.com>
2020-10-09 15:26:17 +0530

    basic firebase authentication added

--- app/src/main/java/com/example/android/medipoint/MainActivity.java
+++ app/src/main/java/com/example/android/medipoint/MainActivity.java
@@ -1,14 +1,113 @@
 package com.example.android.medipoint;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final int RC_SIGN_IN =100 ;
+    private FirebaseAuth mAuth;
+    private String mUsername="ANONYMOUS";
+    TextView textView;
+    private FirebaseAuth.AuthStateListener mFirebaseAuthStateListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth= FirebaseAuth.getInstance();
+        textView=(TextView) findViewById(R.id.username);
+        mFirebaseAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    onSigninInitialized(user.getDisplayName());
+                } else {
+                    onSignoutCleanup();
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+                                    .setAvailableProviders(Arrays.asList(
+                                            new AuthUI.IdpConfig.EmailBuilder().build(),
+                                            new AuthUI.IdpConfig.GoogleBuilder().build()))
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+            }
+        };
+    }
+
+    private void onSignoutCleanup() {
+        mUsername="ANONYMOUS";
+    }
+
+    private void onSigninInitialized(String displayName) {
+        mUsername="Hey! "+displayName;
+        textView.setText(mUsername);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==RC_SIGN_IN)
+        {
+            if(resultCode==RESULT_OK)
+            {
+                Toast.makeText(MainActivity.this,"Signin successfull!",Toast.LENGTH_SHORT).show();
+            }
+            else if(resultCode==RESULT_CANCELED)
+            {
+                Toast.makeText(MainActivity.this,"Signin failed!",Toast.LENGTH_SHORT).show();
+                finish();
+            }
+
+        }
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mAuth.addAuthStateListener(mFirebaseAuthStateListener);
+    }
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        if (mFirebaseAuthStateListener != null) {
+            mAuth.removeAuthStateListener(mFirebaseAuthStateListener);
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.sign_out_menu:
+            {
+                AuthUI.getInstance().signOut(this);
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/android/medipoint/splash_screen.java
+++ app/src/main/java/com/example/android/medipoint/splash_screen.java
@@ -1,5 +1,6 @@
 package com.example.android.medipoint;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
