8f8219fab10c661d2c85e0dbeceaeaf9299f1c30
MERCUROUS <64265450+MERCUROUS@users.noreply.github.com>
2020-10-13 20:04:29 +0530

    activity added

---
+++ app/src/main/java/com/example/android/medipoint/TimeSlot.java
@@ -0,0 +1,14 @@
+package com.example.android.medipoint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TimeSlot extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time_slot);
+    }
+}
